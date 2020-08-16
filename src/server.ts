235270ac8384b8f78d203cd03067646f0afc5375
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-16 18:15:23 -0300

    database docker typeorm

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (request, response) => response.json({ message: 'hello' }));
+
+app.listen(3333);
