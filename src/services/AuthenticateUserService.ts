8a34b349f8caae515f7e733755a9813352194391
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-17 14:05:11 -0300

    token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 export default class AuthenticateUserService {
@@ -30,8 +32,14 @@ export default class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'd181285816ad403f982bed7d6b3fe1a5', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
