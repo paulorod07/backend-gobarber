a974301a59f8781fabddafc9cb7570ecd3241332
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-17 18:05:12 -0300

    handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 import './database';
 
 const app = express();
@@ -13,6 +15,20 @@ app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
-app.get('/', (request, response) => response.json({ message: 'hello' }));
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);
